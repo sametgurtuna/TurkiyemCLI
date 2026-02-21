30b61f68e74b325cccdae0e72d0df65da2f845ea
sametgurtuna <sametgrtna@gmail.com>
2026-02-21 18:00:35 +0300

    chore: update help menu text

--- src/utils/banner.js
+++ src/utils/banner.js
@@ -24,17 +24,20 @@ export function printBanner() {
 export function printHelp() {
   printBanner();
   console.log(chalk.white.bold('  Komutlar:\n'));
-  console.log(chalk.cyan('    turkiyem sehir <ankara|istanbul>') + chalk.gray('  Şehir seç'));
+  console.log(chalk.cyan('    turkiyem sehir <ankara|istanbul|adana|antalya|bursa|izmir|trabzon|samsun>') + chalk.gray('\n                                              Şehir seç'));
   console.log(chalk.cyan('    turkiyem hat <numara>') + chalk.gray('             Hat sorgula'));
-  console.log(chalk.cyan('    turkiyem hat canli <numara> [--detay]') + chalk.gray('  IETT canlı konum'));
-  console.log(chalk.cyan('    turkiyem hava guncel [sehir|lat,lon]') + chalk.gray('  Güncel hava'));
-  console.log(chalk.cyan('    turkiyem hava saatlik [sehir|lat,lon] -g 2') + chalk.gray('  Saatlik tahmin'));
-  console.log(chalk.cyan('    turkiyem hava kalite [sehir|lat,lon]') + chalk.gray('  Hava kalitesi'));
-  console.log(chalk.cyan('    turkiyem deprem son24') + chalk.gray('             Son 24 saat depremler'));
-  console.log(chalk.cyan('    turkiyem deprem 7gun') + chalk.gray('              Son 7 gün depremler'));
-  console.log(chalk.cyan('    turkiyem deprem buyukluk <deger>') + chalk.gray('  Büyüklüğe göre filtrele'));
-  console.log(chalk.cyan('    turkiyem doviz [--tum]') + chalk.gray('            TCMB Döviz kurları'));
-  console.log(chalk.cyan('    turkiyem temizle') + chalk.gray('                  Cache ve config temizle'));
-  console.log(chalk.cyan('    turkiyem --version') + chalk.gray('                Versiyon göster'));
+  console.log(chalk.cyan('    turkiyem hat canli <numara> [--detay]') + chalk.gray('  Canlı araç konumu (IETT)'));
+  console.log(chalk.cyan('    turkiyem durak <id>') + chalk.gray('               Durak bazlı detay sorgula'));
+  console.log(chalk.cyan('    turkiyem hava guncel [sehir|lat,lon]') + chalk.gray('  Güncel hava durumu'));
+  console.log(chalk.cyan('    turkiyem hava saatlik [sehir|lat,lon] -g 2') + chalk.gray('  Saatlik hava tahmini'));
+  console.log(chalk.cyan('    turkiyem hava kalite [sehir|lat,lon]') + chalk.gray('  Hava kalitesi ölçümleri'));
+  console.log(chalk.cyan('    turkiyem deprem son24') + chalk.gray('             Son 24 saatteki depremler'));
+  console.log(chalk.cyan('    turkiyem deprem 7gun') + chalk.gray('              Son 7 gündeki depremler'));
+  console.log(chalk.cyan('    turkiyem deprem buyukluk <deger>') + chalk.gray('  Büyüklüğe göre deprem filtrele'));
+  console.log(chalk.cyan('    turkiyem eczane nobetci [ilce]') + chalk.gray('    Nöbetçi eczaneleri sorgula'));
+  console.log(chalk.cyan('    turkiyem eczane ara <kelime>') + chalk.gray('      Eczane adına veya ilçeye göre ara'));
+  console.log(chalk.cyan('    turkiyem doviz [--tum]') + chalk.gray('            TCMB güncel döviz kurları'));
+  console.log(chalk.cyan('    turkiyem temizle') + chalk.gray('                  Cache ve ayarları temizle'));
+  console.log(chalk.cyan('    turkiyem --version') + chalk.gray('                Versiyonu göster'));
   console.log('');
 }
